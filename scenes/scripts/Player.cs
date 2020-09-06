ec9f5436b059147a92edcde20983087b3ac21e44
Peter <unfinishedideas@gmail.com>
2020-09-06 14:49:14 -0700

    better acceleration!

--- scenes/scripts/Player.cs
+++ scenes/scripts/Player.cs
@@ -8,14 +8,17 @@ public class Player : KinematicBody2D
 {
 	// [Export] public int score = 0;
 
-	[Export] public float currentSpeed = 10f;
+	// [Export] public float currentSpeed = 0f;
 	[Export] public float topSpeed = 660.0f;
-	[Export] public float accel = 10f;
-	[Export] public float decel = 30f;
+	[Export] public float currentAccel = 0f;
+	[Export] public float accel = 30f;
+	[Export] public float decel = 45f;
 
 	[Export] public int jumpForce = 500;
 	[Export] public float gravity = 1100.0f;
 	[Export] public float maxFallSpeed = 4000.0f;
+
+			 public bool facingRight = true;
 
 	Vector2 velocity;
 
@@ -23,50 +26,48 @@ public class Player : KinematicBody2D
 //	{
 //	}
 
-
-
 	public void MoveInputCheck()
 	{
 		if (Input.IsActionPressed("move_right")) {
+			if (!facingRight) {
+				velocity.x = 0;
+				facingRight = true;
+			}
 			if (velocity.x < topSpeed) {
-				currentSpeed += accel;
-				velocity.x += currentSpeed;
-			} else {
-				System.Diagnostics.Debug.WriteLine("topSpeedMaxed");
-				currentSpeed = topSpeed;
-				velocity.x = topSpeed;
+				currentAccel = accel;  // leaving this assignment in for eventual curves. Could just be accel
+				velocity.x += currentAccel;
 			}
-
 		} else if (Input.IsActionPressed("move_left")) {
-			if (Math.Abs(velocity.x) < topSpeed) {
-				currentSpeed += accel;
-				velocity.x -= currentSpeed;
-			} else {
-				System.Diagnostics.Debug.WriteLine("topSpeedMaxed");
-				currentSpeed = topSpeed;
-				velocity.x = -topSpeed;
+			if (facingRight) {
+				velocity.x = 0;
+				facingRight = false;
 			}
-
+			if (velocity.x > -topSpeed) {
+				currentAccel = accel;  // leaving this assignment in for eventual curves. Could just be accel
+				velocity.x -= currentAccel;
+			}
 		} else {
-			if (currentSpeed > 0) {
-				currentSpeed -= decel;
-			}
-
-			if (velocity.x > 0) {
-				velocity.x -= decel;
-			} else if (velocity.x < 0) {
-				velocity.x += decel;
+			currentAccel = decel;  // leaving this assignment in for eventual curves. Could just be decel
+			if (velocity.x < 0) {
+				velocity.x += currentAccel;
+				if (velocity.x > 0) {
+					velocity.x = 0;
+				}
+			} else if (velocity.x > 0) {
+				velocity.x -= currentAccel;
+				if (velocity.x < 0) {
+					velocity.x = 0;
+				}
 			}
 		}
-		
-		// if (Input.IsActionPressed("dash")) {
-		// 	velocity.x *= 1.65f;
-		// }
-		
-		if (Input.IsActionPressed("jump") && IsOnFloor()) {
-			velocity.y -= jumpForce;
-		}
+		// System.Diagnostics.Debug.WriteLine(velocity.x); // <== Console log <== Console log <== Console log <== Console log  
 	}
+
+
+		
+	// 	if (Input.IsActionPressed("jump") && IsOnFloor()) {
+	// 		velocity.y -= jumpForce;
+	// 	}
 
 
 
@@ -76,10 +77,9 @@ public class Player : KinematicBody2D
 		MoveInputCheck();
 		
 		// if you are in the air move faster on x axis
-//		if (Math.Abs(velocity.y) > 30) {
-		if (!IsOnFloor()){ 
-			velocity.x *= 1.5f;
-		}
+		// if (!IsOnFloor()){ 
+		// 	velocity.x *= 1.5f;
+		// }
 		
 		velocity = MoveAndSlide(velocity, new Vector2(0, -1));
 		
@@ -89,7 +89,7 @@ public class Player : KinematicBody2D
 		}
 		
 		// System.Diagnostics.Debug.WriteLine(velocity); // <== Console log <== Console log <== Console log <== Console log  
-		System.Diagnostics.Debug.WriteLine(currentSpeed); // <== Console log <== Console log <== Console log <== Console log  
+		// System.Diagnostics.Debug.WriteLine(currentSpeed); // <== Console log <== Console log <== Console log <== Console log  
 		
 		//	--- animations --- //
 		if (Math.Abs(velocity.x) > 0 && IsOnFloor()) {
